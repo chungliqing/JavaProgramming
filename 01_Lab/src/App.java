2425a4dbfa41e256f08e06b66bebea05eacc98aa
chungliqing <chungliqing21@gmail.com>
2023-01-14 22:01:35 +0800

    lab 01 first commit

---
+++ 01_Lab/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) {
+        
+        
+    }
+
+    public static void firstJavaProgram() {
+        System.out.println("Hello World");
+    }
+
+    public static void displayDifferentStatement() {
+        System.out.println("This is one statement.");
+        System.out.println("This is another statement.");
+    }
+
+    public static void displayNumber() {
+        
+    }
+}
