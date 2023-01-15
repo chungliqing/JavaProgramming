ddfe2df747b61d140ca709ae20c2ab72049c53dc
chungliqing <chungliqing21@gmail.com>
2023-01-15 20:07:37 +0800

    update Lecture 01

--- 01_Lecture/src/App.java
+++ 01_Lecture/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*
+         * Using any text editor, write a Java application that display the message 
+		    Surely the best Java programmers come from Java!
+         * Test your application
+         * Show your app on the board at the beginning of the next lesson
+        */
+
+        System.out.println("Surely the best Java programmers come from Java!");
+
     }
 }
