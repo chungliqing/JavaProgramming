7d580225404db927fbc6f0b2f216021e5a688a51
chungliqing <chungliqing21@gmail.com>
2023-01-16 23:25:36 +0800

    java homework: sort an array in ascending order

--- 05_Lecture/src/App.java
+++ 05_Lecture/src/App.java
@@ -2,10 +2,32 @@ import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
-        var col = new Collection();
+        /*var col = new Collection();
 		int[] numbers = col.readNumbers();
-		col.echoArray(numbers);
+		col.echoArray(numbers);*/
+        sortNumbersInAscendingOrder();
 
+        
+
+
+    }
+
+    public static void sortNumbersInAscendingOrder() {
+        int numbers[] = {100, 99, 98, 97, 96, 95};
+        int original[] = Arrays.copyOfRange(numbers, 0, numbers.length);
+
+        for (int i = 0; i < numbers.length-1; i++){
+            for (int j = i+1; j < numbers.length; j++){
+                if (numbers[j] < numbers[i]){
+                    int temp[] = {numbers[i], numbers[j]};
+                    numbers[i] = temp[1];
+                    numbers[j] = temp[0];
+                }
+            }
+        }
+
+        System.out.println("Original:\n" + Arrays.toString(original));
+        System.out.println(Arrays.toString(numbers));
     }
 }
 //System.out.println(Arrays.toString(numbers)); (return str rep of array's value);
