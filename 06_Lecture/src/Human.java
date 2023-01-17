54d8b455bf600745a24c2f84ea598117a6758319
chungliqing <chungliqing21@gmail.com>
2023-01-17 14:19:26 +0800

    class Human

---
+++ 06_Lecture/src/Human.java
@@ -0,0 +1,19 @@
+public class Human {
+	String name;
+	String gender;
+	int age;
+	double height;
+	double weight;
+
+	public void sleep() {
+	}
+
+	public void eat() {
+	}
+
+	public void run() {
+	}
+	
+	public void drink() {
+	}
+}
