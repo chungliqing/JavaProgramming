571193e80bf9fdcc924ff467cb92e233c82f1d3c
chungliqing <chungliqing21@gmail.com>
2023-02-02 16:32:34 +0800

    08 Lecture

---
+++ 08_Lecture/UserInterface/src/userinterface/UserInterface.java
@@ -0,0 +1,16 @@
+package userinterface;
+
+/**
+ *
+ * @author chung
+ */
+public class UserInterface {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //MyFirstPage obj = new MyFirstPage(); //then a window will pop up
+        MySecondPage obj2 = new MySecondPage();
+        
+    }
+    
+}
